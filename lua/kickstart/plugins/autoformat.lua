e5fa07c0daa4d4f37d0279a2ac3fda06c40688f1
Simen Berge <bergesimen@gmail.com>
2023-08-08 08:39:35 +0200

    null-ls seems to be working

--- lua/kickstart/plugins/autoformat.lua
+++ lua/kickstart/plugins/autoformat.lua
@@ -28,6 +28,40 @@ return {
       return _augroups[client.id]
     end
 
+    -- local status, null_ls = pcall(require, "null-ls")
+    -- if (not status) then return end
+    -- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    --
+    -- local lsp_formatting = function(bufnr)
+    --   vim.lsp.buf.format({
+    --     filter = function(client)
+    --       return client.name == "null-ls"
+    --     end,
+    --     bufnr = bufnr,
+    --   })
+    -- end
+    --
+    -- null_ls.setup {
+    --   sources = {
+    --     null_ls.builtins.formatting.prettierd,
+    --     null_ls.builtins.diagnostics.eslint_d.with({
+    --       diagnostics_format = '[eslint] #{m}\n(#{c})'
+    --     }),
+    --   },
+    --   on_attach = function(client, bufnr)
+    --     if client.supports_method("textDocument/formatting") then
+    --       vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    --       vim.api.nvim_create_autocmd("BufWritePre", {
+    --         group = augroup,
+    --         buffer = bufnr,
+    --         callback = function()
+    --           lsp_formatting(bufnr)
+    --         end,
+    --       })
+    --     end
+    --   end
+    -- }
+
     -- Whenever an LSP attaches to a buffer, we will run this function.
     --
     -- See `:help LspAttach` for more information about this autocmd event.
