a0e4754ea42d0d1ec4f4a5432b04ba24dbb5acf7
Simen Berge <bergesimen@gmail.com>
2023-09-29 20:36:32 +0200

    comment out golines

--- lua/custom/plugins/null-ls.lua
+++ lua/custom/plugins/null-ls.lua
@@ -12,9 +12,9 @@ local M = {
 				null_ls.builtins.diagnostics.eslint_d.with({
 					diagnostics_format = '[eslint] #{m}\n(#{c})'
 				}),
-				null_ls.builtins.formatting.gofumpt,
 				null_ls.builtins.formatting.goimports_reviser,
-				null_ls.builtins.formatting.golines,
+				null_ls.builtins.formatting.gofumpt,
+				-- null_ls.builtins.formatting.golines,
 			},
 			on_attach = function(client, bufnr)
 				if client.supports_method("textDocument/formatting") then
